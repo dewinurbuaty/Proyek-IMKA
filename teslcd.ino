3ef8ef29be24a4657232c16b68d9c669eea1c5aa
Dewi Nurbuaty <fb.depe@gmail.com>
2016-03-04 04:17:00 +0700

    Create teslcd.ino

---
+++ teslcd.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal.h>
+/*
+- lcd rs pin ke digital pin 12
+- lcd enable pin ke digital pin 11
+- lcd d4 pin ke digital pin 5
+- lcd d5 pin ke digital pin 4
+- lcd d6 pin ke digital pin 3
+- lcd d7 pin ke digital pin 2
+
+*/
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+    lcd.begin(16, 2);
+}
+
+void loop() {
+  int i;
+  
+  lcd.setCursor(0, 0);
+  lcd.print("www.boarduino.blogspot.com - Tutorial Arduino");
+  for (i = 0 ; i < 16; i ++) {
+      lcd.scrollDisplayLeft(); 
+      delay(400);
+  } 
+}
